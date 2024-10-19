80eca54ce1b9052330c76469af8260fe45810b41
theQuantumMechanic <albertferguson118@gmail.com>
2024-10-20 02:34:05 +1100

    more tweaks

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+echo $OWNER
+echo $REPO
+
+REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/${OWNER}/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/$DOCKER_USER/action-runner
 
-./config.sh --url https://github.com/${TARGET_REPO_URL} --token ${REG_TOKEN}
+./config.sh --url https://github.com/${OWNER}/${REPO} --token ${REG_TOKEN}
 
 cleanup() {
     echo "Removing runner..."
